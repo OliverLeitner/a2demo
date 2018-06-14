de5ab90f466d43e8456b7bbb021ef3ee4aa0995d
Oliver Leitner <never@neverslair-blog.net>
2018-06-14 21:19:56 +0200

    some fun with yet another interface

--- src/app/guest-form/guest-form.component.ts
+++ src/app/guest-form/guest-form.component.ts
@@ -2,18 +2,52 @@ import { Component, OnInit } from '@angular/core';
 import { Http, Response, HttpModule, Headers, RequestOptions} from '@angular/http';
 import { Router }  from '@angular/router';
 import { Guest } from '../form.model';
+import { Observable } from 'rxjs';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
     selector: 'guest',
     templateUrl: './guest-form.component.html',
-    preserveWhitespaces: true
+    preserveWhitespaces: true,
+    animations: [
+        trigger('visibilityChanged', [
+            state('shown', style({ opacity: 1 })),
+            state('hidden', style({ opacity: 0 })),
+            transition('shown => hidden', animate('600ms')),
+            transition('hidden => shown', animate('300ms')),
+        ])
+    ]
 })
-export class GuestFormComponent {
-    model = new Guest('', '', '', '');
 
-    constructor(private http:Http) {
+//main form page class...
+export class GuestFormComponent {
+    //form handler model variable of custom type Guest
+    //model -> ../form.model.ts
+    model = new Guest('', '', '', '');
+    //for picking the havardart pic, we reserve  string var
+    pic: string;
+
+    //fadein and fadeout of the havardart image
+    visibilityChanged:boolean = true;
+    hideShowAnimation(){
+        this.visibilityChanged = !this.visibilityChanged;
     }
 
+    //the first thing that starts when page loads
+    //so between initial image and the next one there
+    //wont be exactly 10 seconds, then again
+    //if the initial one takes a long time, it does
+    //not interrupt the experience...
+    constructor(private http:Http) {
+        //regrab our havardart every 10 seconds
+        setInterval(() => {
+            this.hideShowAnimation();
+            this.grabRandomArt();
+        }, 10000);
+    }
+
+    //if we submit our form, we post against the given interface and hide the form and show
+    //the submitted message
     onSubmit(user: any) {
         let headers = new Headers();
         headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
@@ -32,8 +66,28 @@ export class GuestFormComponent {
         )
     }
 
-    /*ngOnInit() {
-  }
+    //the function that grabs
+    //a random image url from the havard art archive...
+    grabRandomArt() {
+        this.http.get('https://api.harvardartmuseums.org/image?page=1&sort=random&size=1&q=width:<100&apikey=c6a42ba0-6fe2-11e8-ab88-0dac418e9237')
+            .subscribe(
+                (data) => {
+                    try {
+                        this.pic = data.json().records[0].baseimageurl;
+                    } catch (ex) {
+                        console.log(ex);
+                    }
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+    }
 
-  get currentGuest() { return JSON.stringify(this.model); }*/
+    //called after everything else has been loaded
+    //lazy loading...
+    ngOnInit() {
+        //grab an image to show at page load
+        this.grabRandomArt();
+    }
 }
