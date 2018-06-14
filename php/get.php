76234faf0371fca1cc36d91a62c78cc39ac15513
Oliver Leitner <OliverLeitner@users.noreply.github.com>
2018-06-14 07:40:39 +0200

    Update get.php
    added options method, might be needed for corporate networks

--- php/get.php
+++ php/get.php
@@ -10,5 +10,5 @@ include "loader.php";
 header("Content-Type: application/json, Content-Charset: UTF-8");
 header("Access-Control-Allow-Origin: ".$http_origin);
 header("Access-Control-Allow-Headers: content-type");
-header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
 echo $dbhandler->selectComments();
